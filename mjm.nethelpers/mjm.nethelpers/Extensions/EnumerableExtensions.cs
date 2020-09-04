bd9431876ef139940c47cc305de96b848d6d09dc
Gaburiere <gabriele.coppola@giuneco.it>
2020-09-04 16:53:20 +0200

    isempty/isnullorempty on ienumerable

---
+++ mjm.nethelpers/mjm.nethelpers/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace mjm.nethelpers.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Extension for List.IsNullOrEmpty
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.IsNull() || !enumerable.Any();
+        }
+        
+        /// <summary>
+        /// Extension for List.IsEmpty
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return !enumerable.Any();
+        }
+    }
+}
