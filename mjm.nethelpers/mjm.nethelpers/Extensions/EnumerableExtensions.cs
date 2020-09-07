5fb0cd2b48a8b8e891d3e55c687c23e1a7ad8ddf
Marco <emme.milani@hotmail.it>
2020-09-07 18:23:37 +0200

    Add boolextensions

--- mjm.nethelpers/mjm.nethelpers/Extensions/EnumerableExtensions.cs
+++ mjm.nethelpers/mjm.nethelpers/Extensions/EnumerableExtensions.cs
@@ -9,16 +9,18 @@ namespace mjm.nethelpers.Extensions
         /// Extension for List.IsNullOrEmpty
         /// </summary>
         /// <param name="text"></param>
+        /// <param name="enumerable"></param>
         /// <returns></returns>
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
         {
             return enumerable.IsNull() || !enumerable.Any();
         }
-        
+
         /// <summary>
         /// Extension for List.IsEmpty
         /// </summary>
         /// <param name="text"></param>
+        /// <param name="enumerable"></param>
         /// <returns></returns>
         public static bool IsEmpty<T>(this IEnumerable<T> enumerable)
         {
